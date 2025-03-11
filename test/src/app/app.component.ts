17ff0ec468529b419a7e326ad876a6c099e37f60
arnar1234 <arnark060606@gmail.com>
2025-03-11 12:33:44 +0000

    test

---
+++ test/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  public data: any;
+  title = 'test';
+  isLoggedIn = true;
+  number = 0;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetch();
+  }
+
+  addItem() {
+    this.number += 1;
+  }
+
+  toggle() {
+    this.isLoggedIn = !this.isLoggedIn;
+  }
+
+  public fetch() {
+    this.http.get('https://jsonplaceholder.typicode.com/todos').subscribe(
+      (resp: any) => {
+        this.data = resp;
+      }
+    );
+  }
+}
