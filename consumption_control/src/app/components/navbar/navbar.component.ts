a8444a79ca6f488b50eb5a31e4bbd2c4ca2643c8
Corny-atk <cornelis.attakpa05@gmail.com>
2025-03-12 17:29:38 +0100

    nothing add

--- consumption_control/src/app/components/navbar/navbar.component.ts
+++ consumption_control/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  navLinks = [
+    { path: '/home', label: 'Home' },
+    { path: '/history', label: 'Historique' },
+    { path: '/statistics', label: 'Statistiques' },
+    { path: '/rooms', label: 'Pièces' },
+    { path: '/challenges', label: 'Défis' }
+  ];
 }
